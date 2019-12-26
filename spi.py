09eae7a2a018e3778d30788df5850b085c500aff
Christian Volkmann <christian.volkmann@ammp.io>
2019-12-26 19:49:02 +0100

    Initial commit

---
+++ spi.py
@@ -0,0 +1,26 @@
+from spidev import SpiDev
+
+class SPI:
+    def __init__(self, bus_index, dev_index):
+        self.bus = bus_index
+        self.dev = dev_index
+        self._spi = None
+
+        self._open()
+    
+    def __del__(self):
+        self._close()
+    
+    def _open(self):
+        if not self._spi:
+            self._spi = SpiDev()
+            self._spi.open(self.bus, self.dev)
+            self._spi.max_speed_hz = 8*1000**2
+    
+    def _close(self):
+        if self._spi:
+            self._spi.close()
+            self._spi = None
+        
+    def send(self, data):
+        self._spi.writebytes2(bytes(data))
