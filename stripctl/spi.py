bdb5ba944f7ba7156ae0af95fd602f61648e7a89
Christian Volkmann <christian.volkmann@ammp.io>
2020-02-24 23:05:18 +0100

    Ensure that SPI device exists

--- stripctl/spi.py
+++ stripctl/spi.py
@@ -3,8 +3,13 @@ from typing import Union, List
 
 DEFAULT_FREQ = 8*1000**2 # 8MHz
 
-class SPI:
+class SPIError(RuntimeError):
+    def __init__(self, bus_index: int, dev_index: int):
+        self.bus_index = bus_index
+        self.dev_index = dev_index
+        super().__init__(f'SPI device not found (bus_index={bus_index} device_index={dev_index})')
 
+class SPI:
     def __init__(self, bus_index: int, dev_index: int, freq: int = DEFAULT_FREQ):
         self.bus = bus_index
         self.dev = dev_index
@@ -19,7 +24,10 @@ class SPI:
     def _open(self):
         if not self._spi:
             self._spi = SpiDev()
-            self._spi.open(self.bus, self.dev)
+            try:
+                self._spi.open(self.bus, self.dev)
+            except FileNotFoundError:
+                raise SPIError(self.bus, self.dev)
             self._spi.max_speed_hz = self.freq
     
     def _close(self):
